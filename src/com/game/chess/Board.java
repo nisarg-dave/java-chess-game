0e2348af84eed8b5cee47a595e34d248cf08bc75
Nisarg Dave <ndave630@gmail.com>
2023-10-04 18:42:38 +0800

    Used JFrame and JPanel to create Chess board.

--- src/com/game/chess/Board.java
+++ src/com/game/chess/Board.java
@@ -1,32 +1,37 @@
 package com.game.chess;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Board {
-    Spot[][] squares;
-
     public Board() {
-        squares = new Spot[8][8];
-        squares[0][0] = new Spot(0, 0, new Rook(PieceColour.BLACK));
-        squares[0][1] = new Spot(0, 0, new Horse(PieceColour.BLACK));
-        squares[0][2] = new Spot(0, 0, new Bishop(PieceColour.BLACK));
-        squares[0][3] = new Spot(0, 0, new King(PieceColour.BLACK));
-        squares[0][4] = new Spot(0, 0, new Queen(PieceColour.BLACK));
-        squares[0][5] = new Spot(0, 0, new Bishop(PieceColour.BLACK));
-        squares[0][6] = new Spot(0, 0, new Horse(PieceColour.BLACK));
-        squares[0][7] = new Spot(0, 0, new Rook(PieceColour.BLACK));
+        JFrame frame = new JFrame();
+        frame.setBounds(10, 10, 515, 540);
+        JPanel jPanel = new JPanel() {
+            @Override
+            public void paint(Graphics g) {
+                boolean white = true;
+                // Horizontal Squares
+                for (int y = 0; y < 8; y++) {
+                    // Vertical Squares
+                    for (int x = 0; x < 8; x++) {
+                        if (white) {
+                            g.setColor(Color.white);
+                        } else {
+                            g.setColor(Color.BLACK);
+                        }
+                        // 64 width and 64 height and x and y are the coordinates
+                        g.fillRect(x * 64, y * 64, 64, 64);
+                        white = !white;
+                    }
+                    white = !white;
+                }
+            }
 
-        squares[7][0] = new Spot(0, 0, new Rook(PieceColour.WHITE));
-        squares[7][1] = new Spot(0, 0, new Horse(PieceColour.WHITE));
-        squares[7][2] = new Spot(0, 0, new Bishop(PieceColour.WHITE));
-        squares[7][3] = new Spot(0, 0, new King(PieceColour.WHITE));
-        squares[7][4] = new Spot(0, 0, new Queen(PieceColour.WHITE));
-        squares[7][5] = new Spot(0, 0, new Bishop(PieceColour.WHITE));
-        squares[7][6] = new Spot(0, 0, new Horse(PieceColour.WHITE));
-        squares[7][7] = new Spot(0, 0, new Rook(PieceColour.WHITE));
-
-        for (int i = 0; i < squares.length; i++) {
-            squares[1][i] = new Spot(0, 0, new Pawn(PieceColour.BLACK));
-            squares[6][i] = new Spot(0, 0, new Pawn(PieceColour.WHITE));
-        }
+        };
+        frame.add(jPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
 }
