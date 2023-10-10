4190daaa0914a91a38fc3a9ac287674339acbad0
Nisarg Dave <ndave630@gmail.com>
2023-10-10 21:27:52 +0800

    Able to get Pawns to move

--- src/com/game/chess/Pawn.java
+++ src/com/game/chess/Pawn.java
@@ -10,21 +10,51 @@ public class Pawn extends Piece {
 
     @Override
     public boolean movePiece(int newXPosition, int newYPosition, LinkedList<Piece> pieces) {
-        // If end spot has piece of same colour
-//        if (end.getPiece().getColour() == this.getColour()) {
-//            return;
-//        }
-//
-//        int x = Math.abs(start.getXPosition() - end.getXPosition());
-//        int y = Math.abs(start.getYPosition() - end.getYPosition());
-//
-//        if (x + y == 1 || x + y == 2) {
-//            // If there is a piece of the opposite colour, set it to killed
-//            if (end.getPiece() != null) {
-//                end.getPiece().killPiece();
-//            }
-//            end.setPiece(this);
-//        }
+        Piece pieceInNewPosition = null;
+
+        for (Piece piece : pieces) {
+            // Checking for Pawn of same colour in positions above and returning false
+            // Double Jump
+            if (((this.getColour() == PieceColour.BLACK && this.getYPosition() + 2 == piece.getYPosition()) || (this.getColour() == PieceColour.WHITE && this.getYPosition() - 2 == piece.getYPosition())) && this.getXPosition() == piece.getXPosition() && this.getColour() == piece.getColour()) {
+                return false;
+            }
+            // Single Jump
+            if (((this.getColour() == PieceColour.BLACK && this.getYPosition() + 1 == piece.getYPosition()) || (this.getColour() == PieceColour.WHITE && this.getYPosition() - 1 == piece.getYPosition())) && this.getXPosition() == piece.getXPosition() && this.getColour() == piece.getColour()) {
+                return false;
+            }
+//            // Top Left
+            if ((this.getColour() == PieceColour.BLACK && this.getYPosition() + 1 == piece.getYPosition() && this.getXPosition() - 1 == piece.getXPosition()) || (this.getColour() == PieceColour.WHITE && this.getXPosition() - 1 == piece.getXPosition() && this.getYPosition() - 1 == piece.getYPosition())) {
+                System.out.println("Top Left " + piece.getColour().name() + " " + piece.getName());
+                return false;
+            }
+            // Top Right
+            if ((this.getColour() == PieceColour.BLACK && this.getYPosition() + 1 == piece.getYPosition() && this.getXPosition() + 1 == piece.getXPosition()) || (this.getColour() == PieceColour.WHITE && this.getXPosition() + 1 == piece.getXPosition() && this.getYPosition() - 1 == piece.getYPosition())) {
+                System.out.println("TOP RIGHT " + piece.getColour().name() + " " + piece.getName());
+                return false;
+            }
+            if (piece.getXPosition() == newXPosition && piece.getYPosition() == newYPosition) {
+                pieceInNewPosition = piece;
+            }
+        }
+
+
+        if (pieceInNewPosition != null) {
+            // If end spot has piece of same colour
+            if (pieceInNewPosition.getColour() == this.getColour()) {
+                return false;
+            }
+        }
+
+        int x = Math.abs(newXPosition - this.getXPosition());
+        int y = Math.abs(newYPosition - this.getYPosition());
+
+        if (x + y == 1 || x + y == 2) {
+            // If there is a piece of the opposite colour, set it to killed
+            if (pieceInNewPosition != null) {
+                this.killPiece(pieceInNewPosition);
+            }
+            return true;
+        }
         return false;
     }
 }
