e50b8cd4580b4968fd1095aed317f3677c2b67d5
Nisarg Dave <ndave630@gmail.com>
2023-10-15 12:35:47 +0800

    Able to get Queen to move

--- src/com/game/chess/Queen.java
+++ src/com/game/chess/Queen.java
@@ -10,21 +10,62 @@ public class Queen extends Piece {
 
     @Override
     public boolean movePiece(int newXPosition, int newYPosition, LinkedList<Piece> pieces) {
-        // If end spot has piece of same colour
-//        if (end.getPiece().getColour() == this.getColour()) {
-//            return;
-//        }
-//
-//        int x = Math.abs(start.getXPosition() - end.getXPosition());
-//        int y = Math.abs(start.getYPosition() - end.getYPosition());
-//
-//        if (x - y == 0 || (x + y) % 1 == 0) {
-//            // If there is a piece of the opposite colour, set it to killed
-//            if (end.getPiece() != null) {
-//                end.getPiece().killPiece();
-//            }
-//            end.setPiece(this);
-//        }
+        Piece pieceInNewPosition = null;
+        for (Piece piece : pieces) {
+            // Top
+            if (this.getYPosition() - 1 == piece.getYPosition() && this.getXPosition() == piece.getXPosition() && this.getColour() == piece.getColour() && this.getYPosition() > newYPosition) {
+                return false;
+            }
+            // Bottom
+            if (this.getYPosition() + 1 == piece.getYPosition() && this.getXPosition() == piece.getXPosition() && this.getColour() == piece.getColour() && this.getYPosition() < newYPosition) {
+                return false;
+            }
+            // Right
+            if (this.getXPosition() + 1 == piece.getXPosition() && this.getYPosition() == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() < newXPosition) {
+                return false;
+            }
+            // Left
+            if (this.getXPosition() - 1 == piece.getXPosition() && this.getYPosition() == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() > newXPosition) {
+                return false;
+            }
+            // Top Left Diagonal
+            if (this.getXPosition() - 1 == piece.getXPosition() && this.getYPosition() - 1 == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() > newXPosition && this.getYPosition() > newYPosition) {
+                return false;
+            }
+            // Top Right Diagonal
+            if (this.getXPosition() + 1 == piece.getXPosition() && this.getYPosition() - 1 == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() < newXPosition && this.getYPosition() > newYPosition) {
+                return false;
+            }
+            // Bottom Left Diagonal
+            if (this.getXPosition() - 1 == piece.getXPosition() && this.getYPosition() + 1 == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() > newXPosition && this.getYPosition() < newYPosition) {
+                return false;
+            }
+            // Bottom Right Diagonal
+            if (this.getXPosition() + 1 == piece.getXPosition() && this.getYPosition() + 1 == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() < newXPosition && this.getYPosition() < newYPosition) {
+                return false;
+            }
+            if (piece.getXPosition() == newXPosition && piece.getYPosition() == newYPosition) {
+                pieceInNewPosition = piece;
+            }
+        }
+
+        if (pieceInNewPosition != null) {
+            // If end spot has piece of same colour
+            if (pieceInNewPosition.getColour() == this.getColour()) {
+                return false;
+            }
+        }
+
+        int x = Math.abs(newXPosition - this.getXPosition());
+        int y = Math.abs(newYPosition - this.getYPosition());
+
+        if (x - y == 0 || (x + y) % 1 == 0) {
+            // If there is a piece of the opposite colour, set it to killed
+            if (pieceInNewPosition != null) {
+                this.killPiece(pieceInNewPosition);
+            }
+            return true;
+        }
         return false;
     }
 }
