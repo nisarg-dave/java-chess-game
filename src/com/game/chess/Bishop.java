e42b337839f367244ab4379769f632fd4e2320fb
Nisarg Dave <ndave630@gmail.com>
2023-10-15 12:50:17 +0800

    Added check for newXPosition and newYPosition when moving

--- src/com/game/chess/Bishop.java
+++ src/com/game/chess/Bishop.java
@@ -15,19 +15,19 @@ public class Bishop extends Piece {
         // If there are same colour pieces in the first diagonal then return false
         for (Piece piece : pieces) {
             // Top Left Diagonal
-            if (this.getXPosition() - 1 == piece.getXPosition() && this.getYPosition() - 1 == piece.getYPosition() && this.getColour() == piece.getColour()) {
+            if (this.getXPosition() - 1 == piece.getXPosition() && this.getYPosition() - 1 == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() > newXPosition && this.getYPosition() > newYPosition) {
                 return false;
             }
             // Top Right Diagonal
-            if (this.getXPosition() + 1 == piece.getXPosition() && this.getYPosition() - 1 == piece.getYPosition() && this.getColour() == piece.getColour()) {
+            if (this.getXPosition() + 1 == piece.getXPosition() && this.getYPosition() - 1 == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() < newXPosition && this.getYPosition() > newYPosition) {
                 return false;
             }
             // Bottom Left Diagonal
-            if (this.getXPosition() - 1 == piece.getXPosition() && this.getYPosition() + 1 == piece.getYPosition() && this.getColour() == piece.getColour()) {
+            if (this.getXPosition() - 1 == piece.getXPosition() && this.getYPosition() + 1 == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() > newXPosition && this.getYPosition() < newYPosition) {
                 return false;
             }
             // Bottom Right Diagonal
-            if (this.getXPosition() + 1 == piece.getXPosition() && this.getYPosition() + 1 == piece.getYPosition() && this.getColour() == piece.getColour()) {
+            if (this.getXPosition() + 1 == piece.getXPosition() && this.getYPosition() + 1 == piece.getYPosition() && this.getColour() == piece.getColour() && this.getXPosition() < newXPosition && this.getYPosition() < newYPosition) {
                 return false;
             }
             if (piece.getXPosition() == newXPosition && piece.getYPosition() == newYPosition) {
